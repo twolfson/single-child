8616900ba335e0494179c1e1708f5f7bc36cdb6b
Todd Wolfson <todd@twolfson.com>
2013-06-26 21:25:21 -0700

    Broke apart test into doubleshot test

---
+++ test/single-child_test.content.js
@@ -0,0 +1,66 @@
+// Load in dependencies
+var SingleChild = require('../lib/single-child.js'),
+    fs = require('fs'),
+    assert = require('assert');
+
+// Cleanup temporary files before starting
+try { fs.unlinkSync('tmp.txt'); } catch (e) {}
+
+module.exports = {
+  // Generic namespace
+  'A SingleChild': function () {
+  },
+
+  // Commands for the first test
+  'running a self-terminating command': function () {
+    // Create a script that writes time to `tmp.txt`
+    this.child = new SingleChild('node', ['-e', 'require("fs").writeFileSync("tmp.txt", +new Date())']);
+  },
+  'when started': function (done) {
+    // Start the child
+    this.child.start();
+
+    // Callback in a bit
+    setTimeout(done, 100);
+  },
+  'runs the command': function () {
+    // Load in the time
+    var content = fs.readFileSync(__dirname + '/../tmp.txt', 'utf8');
+
+    // Assert something is there
+    assert.notEqual(content, '');
+
+    // Save that something for later
+    this.content1 = content;
+  },
+  'when started again': function (done) {
+    // Start the child
+    this.child.restart();
+
+    // Callback in a bit
+    setTimeout(done, 100);
+  },
+  'runs the command again': function () {
+    // Load in the time
+    var content = fs.readFileSync(__dirname + '/../tmp.txt', 'utf8');
+
+    // Assert something is there
+    assert.notEqual(content, this.content1);
+  },
+
+  // Commands for the second command
+  'running a sustaining command': function () {
+    // Create a script that writes time to `tmp.txt`
+    this.child = new SingleChild('node', [
+      '-e',
+      [
+        'var startTime = (+new Date()) + "";',
+        'require("http").createServer(function (req, res) {',
+        '  res.writeHead(200);',
+        '  res.write(startTime);',
+        '  res.end();',
+        '}).listen(3000);'
+      ].join('\n')
+    ]);
+  }
+};
